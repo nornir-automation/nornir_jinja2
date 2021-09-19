ef9c45487ddaa2429586a25cf419c34d33c3588a
jifox <josef.fuchs@j-fuchs.at>
2021-09-19 05:58:49 +0200

    Add test: test_jinja_filter

--- tests/unit/test_template_string.py
+++ tests/unit/test_template_string.py
@@ -15,6 +15,14 @@ my_var: {{ my_var}}
 
 """
 
+simple2_j2 = """
+
+host-name: {{ host }}
+
+my_var: {{ my_var | to_upper }}
+
+"""
+
 broken_j2 = """
 #Broken template
 
@@ -23,6 +31,8 @@ host-name {{ host
 my_var: {{ my_var}}
 """
 
+def jinja_filter_to_upper(value):
+    return str(value).upper()
 
 class Test(object):
     def test_template_string(self, nr):
@@ -36,6 +46,22 @@ class Test(object):
                 assert "my_var: comes_from_host1.group_1" in r.result
             if h == "host2.group_1":
                 assert "my_var: comes_from_group_1" in r.result
+
+    def test_jinja_filter(self, nr):
+        filters = {
+            "to_upper": jinja_filter_to_upper,
+        }
+        result = nr.run(template_string, template=simple2_j2, my_var="asd", jinja_filters=filters)
+
+        assert result
+        for h, r in result.items():
+            assert "host-name: {}".format(h) in r.result
+            if h == "host1.group_1":
+                assert "my_var: COMES_FROM_HOST1.GROUP_1" in r.result
+            if h == "host2.group_1":
+                assert "my_var: COMES_FROM_GROUP_1" in r.result
+            if h == "dev3.group_2":
+                assert "my_var: ASD" in r.result
 
     def test_template_string_error_broken_string(self, nr):
         results = nr.run(template_string, template=broken_j2)
