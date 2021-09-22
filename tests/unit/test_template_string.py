7ccea50e5b4e5d094047248a586f6c76b1e3f7be
jifox <jifox@users.noreply.github.com>
2021-09-22 20:01:25 +0200

    Add test: test_jinja_filter (#9)

--- tests/unit/test_template_string.py
+++ tests/unit/test_template_string.py
@@ -1,8 +1,7 @@
 import os
 
-from nornir_jinja2.plugins.tasks import template_string
-
 from jinja2 import TemplateSyntaxError
+from nornir_jinja2.plugins.tasks import template_string
 
 
 data_dir = "{}/test_data".format(os.path.dirname(os.path.realpath(__file__)))
@@ -15,6 +14,14 @@ my_var: {{ my_var}}
 
 """
 
+simple2_j2 = """
+
+host-name: {{ host }}
+
+my_var: {{ my_var | to_upper }}
+
+"""
+
 broken_j2 = """
 #Broken template
 
@@ -22,6 +29,10 @@ host-name {{ host
 
 my_var: {{ my_var}}
 """
+
+
+def jinja_filter_to_upper(value):
+    return str(value).upper()
 
 
 class Test(object):
@@ -36,6 +47,24 @@ class Test(object):
                 assert "my_var: comes_from_host1.group_1" in r.result
             if h == "host2.group_1":
                 assert "my_var: comes_from_group_1" in r.result
+
+    def test_jinja_filter(self, nr):
+        filters = {
+            "to_upper": jinja_filter_to_upper,
+        }
+        result = nr.run(
+            template_string, template=simple2_j2, my_var="asd", jinja_filters=filters
+        )
+
+        assert result
+        for h, r in result.items():
+            assert "host-name: {}".format(h) in r.result
+            if h == "host1.group_1":
+                assert "my_var: COMES_FROM_HOST1.GROUP_1" in r.result
+            if h == "host2.group_1":
+                assert "my_var: COMES_FROM_GROUP_1" in r.result
+            if h == "dev3.group_2":
+                assert "my_var: ASD" in r.result
 
     def test_template_string_error_broken_string(self, nr):
         results = nr.run(template_string, template=broken_j2)
