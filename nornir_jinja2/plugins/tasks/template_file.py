17a92bd8e18f75604432ef3e3ba3a9348d588409
seisokeii <phlin@it.cs.nycu.edu.tw>
2024-07-07 16:26:45 +0800

    Add json xml format arguments

--- nornir_jinja2/plugins/tasks/template_file.py
+++ nornir_jinja2/plugins/tasks/template_file.py
@@ -4,6 +4,11 @@ from nornir.core.task import Result, Task
 
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
+import json
+import xml.etree.ElementTree as elementTree
+import xml.dom.minidom as minidom
+import re
+
 FiltersDict = Optional[Dict[str, Callable[..., str]]]
 
 
@@ -11,6 +16,8 @@ def template_file(
     task: Task,
     template: str,
     path: str,
+    format_json = False,
+    format_xml = False,
     jinja_filters: Optional[FiltersDict] = None,
     jinja_env: Optional[Environment] = None,
     **kwargs: Any
@@ -23,6 +30,8 @@ def template_file(
         path: path to dir with templates
         jinja_filters: jinja filters to enable. Defaults to nornir.config.jinja2.filters
         jinja_env: A fully configured jinja2 environment
+        format_json: formats rendered JSON files containing extra whitespace or indentation
+        format_xml: formats rendered XML files containing extra whitespace or indentation
         **kwargs: additional data to pass to the template
 
     Returns:
@@ -42,4 +51,35 @@ def template_file(
     t = env.get_template(template)
     text = t.render(host=task.host, **kwargs)
 
+
+    if format_json and is_json(text):
+        reform = json.dumps(json.loads(text), indent=4)
+
+        return Result(host=task.host, result=reform)
+
+
+    if format_xml and is_xml(text):
+        reform = minidom.parseString(text).toprettyxml(indent="      ")
+        reform = re.sub(r'^<\?xml.*?\?>', '', reform).strip()
+        reform = re.sub(r'\n\s*\n', '\n', reform)
+        reform = re.sub(r'&quot;', '"', reform)
+
+        return Result(host=task.host, result=reform)
+
     return Result(host=task.host, result=text)
+
+
+def is_json(text):
+    try:
+        json.loads(text)
+    except ValueError as e:
+        return False
+    return True
+
+
+def is_xml(text):
+    try:
+        elementTree.fromstring(text)
+    except elementTree.ParseError as e:
+        return False
+    return True
